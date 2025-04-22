f3e36265f0beea012df760be2a0a1d7ac1652fd0
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-04-22 14:19:52 -0300

    Adicionando a exclusão do filme da lista de favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,9 +10,19 @@ function Favoritos() {
     setFilmes(JSON.parse(minhaLista) || []);
   }, []);
 
+  function excluirFilme(id){
+    let filtroFilmes = filmes.filter((item) => {
+      return (item.id !== id)
+    })
+    setFilmes(filtroFilmes)
+    localStorage.setItem('@primeflix', JSON.stringify(filtroFilmes))
+  }
+
   return (
     <div className="meus-filmes">
       <h1>Meus Filmes</h1>
+
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo😕</span>}
       <ul>
         {filmes.map((item) => {
           return(
@@ -20,7 +30,7 @@ function Favoritos() {
                 <span>{item.title}</span>
                 <div>
                     <Link to={`/filmes/${item.id}`}>Ver detalhes</Link>
-                    <button>Excluir</button>
+                    <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                 </div>
             </li>
           )
